9e8a04abd3766eb9cfbc3c89115bb4c5615ec2e9
rynqova-studio <rurivo.business@gmail.com>
2026-09-02 00:22:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.getElementById('menu').onclick=()=>document.getElementById('nav').classList.toggle('open');document.querySelectorAll('#nav a').forEach(a=>a.onclick=()=>document.getElementById('nav').classList.remove('open'));
